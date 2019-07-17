b49641720abe8a8c687d24c6e623ca71ade1b0c7
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-17 02:43:26 +0000

    added contract to React

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,38 @@ import logo from './logo.svg';
 import Web3 from 'web3';
 import './App.css';
 
-const web3 = new Web3('https://9545-fdf183c2-eac9-47d3-8b15-9c3a1043eb38.ws-us0.gitpod.io');
+const web3 = new Web3('https://9545-fdf183c2-eac9-47d3-8b15-9c3a1043eb38.ws-us0.gitpod.io', null);
+
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "uint256"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "get",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 function App() {
   const [number, setNumber] = useState(0);
@@ -14,8 +45,13 @@ function App() {
 
     const accounts = await window.ethereum.enable();
     const account = accounts[0];
-    console.log(account);
-    // let instance = await SimpleStorage.deployed()
+    //console.log(account);
+
+    const contractAddress = '0x23B4E1c0dbD1E93B0d8e21c05fDb1FF1ADEDC3D1';
+
+    const storageContract = web3.eth.Contract(abi,contractAddress);
+
+    console.log(storageContract);
   }
 
   console.log(web3);
