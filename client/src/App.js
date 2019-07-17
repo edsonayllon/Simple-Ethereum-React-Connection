96d723b6864597eec3a8697203d61f28038baa2a
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-17 02:14:12 +0000

    added rpc for this instance

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
+import Web3 from 'web3';
 import './App.css';
+
+const web3 = new Web3('https://9545-fdf183c2-eac9-47d3-8b15-9c3a1043eb38.ws-us0.gitpod.io');
 
 function App() {
   const [number, setNumber] = useState(0);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log('ran');
 
-
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    console.log(account);
+    // let instance = await SimpleStorage.deployed()
   }
 
+  console.log(web3);
   console.log(number)
 
   return (
