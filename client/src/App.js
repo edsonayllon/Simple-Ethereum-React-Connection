9af35ecd31739d1e3666838d894009d350123304
Edson Ayllon <eayllon1@gmail.com>
2019-12-25 23:52:56 -0500

    small update

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,6 @@ function App() {
 
   const handleGet = async (e) => {
     e.preventDefault();
-    await window.ethereum.enable();
     const result = await SimpleContract.methods.get().call();
     setGetNumber(result);
     console.log(result);
